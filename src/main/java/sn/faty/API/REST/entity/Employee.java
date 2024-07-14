b2935cba5ee1bc7405c987494f865a159ea1307b
FATY <laminefaty5261@gmail.com>
2024-07-14 05:58:17 +0200

    suppresion du fichier charge du workflow

---
+++ src/main/java/sn/faty/API/REST/entity/Employee.java
@@ -0,0 +1,32 @@
+package sn.faty.API.REST.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
